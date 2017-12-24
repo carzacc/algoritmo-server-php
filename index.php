c7ccd5756b3506f419f0fa1ee0c5d95ae72ea11c
Carmine <zaccagnino@hotmail.it>
2017-12-24 19:18:25 +0100

    Ora l'output è JSON

--- index.php
+++ index.php
@@ -1,11 +1,16 @@
  <?php
 include 'algoritmo.php';
+header('Content-Type: application/json');
 $g = ($_GET['g'] ?: 15);
 partite($g);
+$arrsquadre = [];
 foreach ($squadre as $squadra)	{
-	print($squadra->getNome());
-	print ": ";
-	print($squadra->getPunti());
-	print "\n<br>";
+	$o->Squadra = $squadra->getNome();
+	$o->Alternativa = $squadra->getPunti();
+	$o->Tradizionale = $squadra->getPuntiTrad();
+	$o->Somma = $squadra->getPunti()+$squadra->getPuntiTrad();
+	array_push($arrsquadre, $o);
 }
+$out = json_encode($arrsquadre);
+print $out."\n";
 ?>
